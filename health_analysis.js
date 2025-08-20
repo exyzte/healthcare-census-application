eb1b1671a67a80574a403e33bd339f35d1ff6d2e
exyzte <davidecicerys@gmail.com>
2025-08-19 22:02:52 -0500

    updated js

--- health_analysis.js
+++ health_analysis.js
@@ -1,3 +1,13 @@
 
 xhr = new XMLHttpRequest();
 xhr.open("GET", "/.health_analysis.json", true);
+xhr.onload = function() {
+    if(xhr.status >= 200 && xhr.status < 300) {
+
+    }
+}
+
+const addPatientButton = document.getElementById("addPatient");
+const report = document.getElementById("report");
+const btnSearch = document.getElementById("btnSearch");
+const patients = [];
